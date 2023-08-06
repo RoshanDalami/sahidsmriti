b94a26fb2b6784fc8c1c19db120dc7bd36d812dc
rsunar <tech.channel.002@gmail.com>
2023-08-06 14:51:02 +0530

    conditional layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,46 @@
-
+"use client";
 
 import "./globals.css";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./Components/Navbar";
 import { CssBaseline } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Toaster } from 'react-hot-toast'
-
+import { Toaster } from "react-hot-toast";
+import { useParams } from "next/navigation";
 import FooterPage from "./Components/Footer";
-
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Sahid Smriti Samudayiak Hospital",
-  description: "",
-};
+// export const metadata: any = {
+//   title: "Sahid Smriti Samudayiak Hospital",
+//   description: "",
+// };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const {id} = useParams();
+  console.log(id)
+  console.log(pathname);
   return (
     <html lang="en">
       <CssBaseline />
-      <Toaster position='top-center' />
       <body className={inter.className}>
-        <NavBar />
+        <Toaster position="top-center" />
+
+        {(pathname === "/appointment" || pathname === `appointment/${id}`) ? null : (
+          <NavBar />
+        )}
 
         {children}
-        <FooterPage />
+        {(pathname === "/appointment" || pathname === `appointment/${id}`) ? null : (
+          <FooterPage />
+        )}
       </body>
     </html>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request:NextRequest){
     const token = request.cookies.get('token')?.value || ''
 
     if(isPublicPath && token){
-        return NextResponse.redirect(new URL('/warning',request.nextUrl))
+        return NextResponse.redirect(new URL('/',request.nextUrl))
     }
     if(!isPublicPath && !token){
         return NextResponse.redirect(new URL('/login',request.nextUrl))
@@ -30,6 +30,6 @@ export const config = {
       '/staff',
       '/appointment',
       '/signup',
-      '/warning',
+      
     ],
   }
