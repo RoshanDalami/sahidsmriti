df06299f5a5d042e4da474f61de6fe6dd3a4cc47
rsunar <tech.channel.002@gmail.com>
2023-08-07 16:37:10 +0530

    fixing slow rendering

--- src/app/Components/GalleryCarousel.tsx
+++ src/app/Components/GalleryCarousel.tsx
@@ -5,17 +5,10 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import image01 from "../../../public/assets/Dental01.jpeg";
-import image02 from "../../../public/assets/dental.jpeg";
-import otImage1 from '../../../public/assets/operation01.jpeg'
-import otImage2 from '../../../public/assets/operation02.jpeg'
-import otImage3 from '../../../public/assets/operation03.jpeg'
+import { nanoid } from "nanoid";
 
 
-export default function CarouselGallery() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+export default function CarouselGallery(props:any) {
   const config = {
     dots: false,
     infinite: true,
@@ -53,45 +46,26 @@ export default function CarouselGallery() {
     ],
   };
 
-  const DentalImage = [image01, image02, image01, image02];
-
-  const itemDental = DentalImage.map((image, index) => {
-    return (
-      <div className="shadow  transition-all px-3 "  key={index}>
-        <Image
-          src={image}
-          alt=""
-          className=" object-cover h-96 mx-10 rounded "
-        />
-        </div>
-      
-    );
-  });
-  const OTImage = [otImage1,otImage2,otImage3,otImage2,otImage3]
-  const itemOT = OTImage.map((image, index) => {
-    return (
-      <div className="shadow  transition-all px-3 " key={index}>
-        <Image
-          src={image}
-          alt=""
-          className=" object-cover h-96 mx-10 rounded "
-        />
-        </div>
-      
-    );
-  });
+  
 
   return (
-    <div className="flex flex-col items-center justify-center mb-20">
+    <div className="flex flex-col items-center justify-center mb-10">
       <div className="w-full">
-        <h1 className="text-center text-4xl my-4">Dental Gallery</h1>
-        <Slider {...config}>{itemDental}</Slider>
-      </div>
-      <div className="w-full">
-        <h1 className="text-center text-4xl my-4">Operation Theater Gallery</h1>
-        <Slider {...config}>{itemOT}</Slider>
+        <h1 className="text-center text-4xl my-4">{props.title}</h1>
+        <Slider {...config}>
+          {props.data.map((image:ImageOrientation) => {
+            return (
+              <div className="shadow  transition-all px-3 " key={nanoid()}>
+                <Image
+                  src={image}
+                  alt=""
+                  className=" object-cover h-96 mx-10 rounded "
+                />
+              </div>
+            );
+          })}
+        </Slider>
       </div>
     </div>
   );
 }
-
