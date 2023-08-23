5eda9c57853414a9cc4001330337a2ef56d8d1d6
RoshanDalami <roshandalami0@gmail.com>
2023-08-23 16:30:36 +0545

    on client

--- src/app/(public)/events/[id]/page.tsx
+++ src/app/(public)/events/[id]/page.tsx
@@ -42,7 +42,7 @@ export default function Event() {
   return (
     <React.Fragment>
 
-      <h1 className="font-bold text-3xl text-black text-center m-5">New & Event</h1>
+      <h1 className="font-bold text-3xl text-black text-center m-5">News & Events</h1>
 
       <div className="flex items-center flex-col justify-center ">
         {
