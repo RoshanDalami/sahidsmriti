3eccc015760d3ac25e1504c9fe2f675c5fc0d56a
RoshanDalami <roshandalami0@gmail.com>
2023-08-24 13:39:32 +0545

    added description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sahid Smriti Samudayiak Hospital",
-  description: "",
+  description: "The Sahid Smriti Samudayik Hospital was established in 2067 BS driven by the Vision to provide best medical healthcare to the people of Chitwan and its neighboring district so that the people be treated well as they were compelled to travel out of even for minor treatment. As the overwhelming response it received from the people of this region, the hospital became one of the best hospital to provide finest healthcare in a pleasant environment by professional medical team.",
 };
 
 
